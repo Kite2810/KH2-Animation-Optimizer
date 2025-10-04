e3ee299b662a2a8bde4cd61770875cac13ecf7ce
Kité <kitekinonvip@gmail.com>
2025-10-05 00:12:22 +0200

    update

--- KH2 Animation Optimizer.py
+++ KH2 Animation Optimizer.py
@@ -1,10 +1,10 @@
 bl_info = {
     "name": "KH2 Animation Optimizer",
     "author": "Kité",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (3, 0, 0),
     "location": "3D Viewport > Sidebar > KH2 Anb Optimizer",
-    "description": "Adjust the Frame Rate to 15, changes Interpolation for all Keyframes to Bezir and changes Handle Type to Vector. Also Moves all Keyframes to 1, 1.5, 2, 2.5 and so on.",
+    "description": "Moves all Keyframes to evenly spaced intervals (1, 1.5, 2, 2.5, etc.)",
     "category": "Animation",
 }
 
@@ -21,12 +21,12 @@ class EvenlyStaggerProperties(PropertyGroup):
     )
     spacing: FloatProperty(
         name="Frame spacing",
-        default=0.5,
+        default=2.0,
         description="Time interval between the keyframes"
     )
     fps: IntProperty(
         name="New Frame Rate",
-        default=15,
+        default=60,
         min=1,
         description="Frame Rate"
     )
@@ -35,7 +35,7 @@ class EvenlyStaggerProperties(PropertyGroup):
 class ANIM_OT_evenly_stagger_keyframes(Operator):
     bl_idname = "anim.evenly_stagger_keyframes"
     bl_label = "Evenly distribute keyframes"
-    bl_description = "Adjust the Frame Rate to 15, changes Interpolation for all Keyframes to Bezir and changes Handle Type to Vector. Also Moves all Keyframes to 1, 1.5, 2, 2.5 and so on."
+    bl_description = "Moves all Keyframes to evenly spaced intervals"
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
@@ -43,7 +43,7 @@ class ANIM_OT_evenly_stagger_keyframes(Operator):
         props = context.scene.evenly_stagger_props
 
         if not obj or not obj.animation_data or not obj.animation_data.action:
-            self.report({'ERROR'}, "Kein animiertes Objekt gefunden.")
+            self.report({'ERROR'}, "No Animated Armature found.")
             return {'CANCELLED'}
 
         action = obj.animation_data.action
@@ -54,11 +54,6 @@ class ANIM_OT_evenly_stagger_keyframes(Operator):
             for i, key in enumerate(keyframes):
                 new_frame = props.start + (i * props.spacing)
                 key.co.x = new_frame
-                key.handle_left.x = new_frame
-                key.handle_right.x = new_frame
-                key.interpolation = 'BEZIER'
-                key.handle_left_type = 'VECTOR'
-                key.handle_right_type = 'VECTOR'
 
         context.scene.render.fps = props.fps
         self.report({'INFO'}, f"Keyframes redistributed. FPS = {props.fps}")
